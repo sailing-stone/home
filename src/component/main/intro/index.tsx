0ed2a7177cdaf1f93792c485ff24a055f5c58236
dhgg321 <ihji@sailingstone.io>
2025-01-02 15:48:12 +0900

    feat: 메인 페이지 인트로 컴포넌트 개발

---
+++ src/component/main/intro/index.tsx
@@ -0,0 +1,23 @@
+import Image from '@/component/base/Image';
+
+const Intro = () => {
+  return (
+    <section className='flex items-center justify-center gap-20 px-[10%] pb-80 pt-40'>
+      <Image
+        width='48.5rem'
+        height='72.7rem'
+        src='/image/main-intro.png'
+        alt='인트로'
+      />
+      <div className='flex flex-col gap-20 text-common-black'>
+        <h3 className='text-[4rem] font-extrabold'>WITH SAILING STONE.</h3>
+        <p className='text-[3.5rem] font-medium'>
+          세일링 스톤의 데이터 및 AI 솔루션을 통해
+          <br /> 데이터에 대한 투자 성과를 확인해 보세요.
+        </p>
+      </div>
+    </section>
+  );
+};
+
+export default Intro;
