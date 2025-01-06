962507dfc6ca653a4cb68b4a797be7b7011aa47d
dhgg321 <ihji@sailingstone.io>
2025-01-06 15:23:01 +0900

    feat: 리쿠르트 TBD 컴포넌트 개발

--- src/component/recruit/TBD/index.tsx
+++ src/component/recruit/TBD/index.tsx
@@ -1,16 +1,18 @@
-/* eslint-disable tailwindcss/no-custom-classname */
 import Badge from '@/component/base/badge';
 
 const TBD = () => {
   return (
-    <section>
+    <section className='flex flex-col items-center justify-center gap-12 px-20 py-80'>
       <Badge>
         <h1 className='section-h1'>인재상 or 채용 절차 (TBD)</h1>
       </Badge>
-      <small>
+      <small className='text-[2rem] font-normal'>
         Invite shadow community opacity variant. Outline blur subtract content
         ellipse.
       </small>
+      <div className='mt-20 flex h-[49.8rem] w-[116.8rem] items-center justify-center bg-[#D9D9D9] text-[8rem]'>
+        TBD
+      </div>
     </section>
   );
 };
