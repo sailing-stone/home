35ac31c2d2fac15b56ef38f6f188a0af34c2b42e
dhgg321 <ihji@sailingstone.io>
2025-01-14 17:29:49 +0900

    modify: 리쿠르트 배너 비디오로 변경

--- src/component/recruit/banner/index.tsx
+++ src/component/recruit/banner/index.tsx
@@ -1,23 +1,28 @@
-import BannerImage from '@/asset/image/recruit/recruit-banner.png';
+import BannerVideo from '@/asset/video/recruit-banner.mp4';
 
 const Banner = () => {
   return (
-    <section
-      className='relative flex aspect-[16/9] max-h-[72rem] w-full items-end bg-cover bg-center bg-no-repeat px-[9.9rem] py-[7.1rem] text-common-white'
-      style={{ backgroundImage: `url(${BannerImage})` }}
-    >
-      <div className='absolute inset-0 bg-black/50' />
-      <div className='relative z-10'>
-        <div className='flex flex-col gap-12 text-common-white'>
+    <section className='relative text-common-white'>
+      <video
+        src={BannerVideo}
+        autoPlay
+        loop
+        preload='auto'
+        muted
+        className='max-h-[72rem] w-full object-cover'
+      />
+      <div className='absolute bottom-[7.1rem] left-[9.9rem] flex flex-col gap-12'>
+        <div>
+          <p className='text-[3rem] font-medium leading-[3.6rem]'>_</p>
           <h1 className='banner-h1'>리쿠르트 (인재 모집... TBD)</h1>
-          <p className='text-[2rem] font-light leading-[3rem]'>
-            Lorem ipsum dolor sit amet consectetur. Pharetra volutpat amet
-            libero sem viverra eget tempor in non. Egestas purus eget tellus
-            augue vestibulum ut sed curabitur. Non commodo dictum scelerisque
-            cras tortor egestas nulla. Sit cursus sagittis sociis pretium
-            euismod vestibulum id.
-          </p>
         </div>
+        <p className='text-[2rem] font-light leading-[3rem]'>
+          Lorem ipsum dolor sit amet consectetur. Pharetra volutpat amet libero
+          sem viverra eget tempor in non. Egestas purus eget tellus augue
+          vestibulum ut sed curabitur. Non commodo dictum scelerisque cras
+          tortor egestas nulla. Sit cursus sagittis sociis pretium euismod
+          vestibulum id.
+        </p>
       </div>
     </section>
   );
