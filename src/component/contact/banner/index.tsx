5269164368135b2831b21d05e3694c43fedf68e9
dhgg321 <ihji@sailingstone.io>
2025-01-14 17:33:46 +0900

    modify: 문의 페이지 배너 동영상 수정

--- src/component/contact/banner/index.tsx
+++ src/component/contact/banner/index.tsx
@@ -1,19 +1,24 @@
-import BannerImage from '@/asset/image/contact/contact-banner.png';
+import BannerVideo from '@/asset/video/contact-banner.mp4';
 
 const Banner = () => {
   return (
-    <section
-      className='relative flex aspect-[16/9] max-h-[72rem] w-full items-end bg-cover bg-center bg-no-repeat px-[9.9rem] py-[9.4rem] text-common-white'
-      style={{ backgroundImage: `url(${BannerImage})` }}
-    >
-      <div className='absolute inset-0 bg-black/50' />
-      <div className='relative z-10'>
-        <div className='flex flex-col gap-12 text-common-white'>
+    <section className='relative text-common-white'>
+      <video
+        src={BannerVideo}
+        autoPlay
+        loop
+        preload='auto'
+        muted
+        className='max-h-[72rem] w-full object-cover'
+      />
+      <div className='absolute bottom-[7.1rem] left-[9.9rem] flex flex-col gap-12'>
+        <div>
+          <p className='text-[3rem] font-medium leading-[3.6rem]'>_</p>
           <h1 className='banner-h1'>문의하기</h1>
-          <p className='text-[2rem] font-light leading-[3rem]'>
-            파트너십 문의 기타 문의 등등
-          </p>
         </div>
+        <p className='text-[2rem] font-light leading-[3rem]'>
+          파트너십 문의 기타 문의 등등
+        </p>
       </div>
     </section>
   );
