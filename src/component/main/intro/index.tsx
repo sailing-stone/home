c97c9dfae3dd242d26a604c32aeb36bc110dd9e7
dhgg321 <ihji@sailingstone.io>
2025-02-03 13:03:38 +0900

    style: 메인 페이지 인트로 반응형

--- src/component/main/intro/index.tsx
+++ src/component/main/intro/index.tsx
@@ -1,59 +1,25 @@
-import { AnimatePresence, motion } from 'framer-motion';
-import { twJoin } from 'tailwind-merge';
 import MainIntroImage_1 from '@/asset/image/main/main-intro_1.png';
-import MainIntroImage_2 from '@/asset/image/main/main-intro_2.png';
 import Image from '@/component/base/Image';
-import useCount from '@/hook/useCount';
-import useInterval from '@/hook/useInterval';
-
-const CONTENT = [
-  <>
-    세일링 스톤의 데이터 및 AI 솔루션을 통해
-    <br /> 데이터에 대한 투자 성과를 확인해 보세요.
-  </>,
-  <>
-    AI 기술과 함께 당신의 데이터에
-    <br /> 가치를 더해 드립니다.
-  </>,
-];
 
 const Intro = () => {
-  const { count, increase } = useCount(CONTENT.length - 1);
-  useInterval(increase, 3000);
-
   return (
-    <section className='flex items-center justify-center gap-[5.1rem] pb-[27.2rem] pt-[11.2rem]'>
+    <section className='flex items-center justify-center gap-12 py-60 mobile:flex-col mobile:py-32 tablet:py-40'>
+      <div className='flex flex-col gap-[3.2rem] self-end text-right text-common-black mobile:gap-[1.6rem] mobile:self-center'>
+        <h3 className='title-40 font-bold leading-[4.8rem] mobile:leading-[2.9rem] tablet:leading-[4.2rem]'>
+          WITH <br />
+          SAILING STONE
+        </h3>
+        <p className='title-28 font-medium leading-[3.3rem] mobile:leading-[1.9rem] tablet:leading-[2.9rem]'>
+          세일링 스톤의 데이터 및 AI 솔루션을 통해
+          <br /> 데이터에 대한 투자 성과를 확인해 보세요.
+        </p>
+      </div>
       <Image
-        width='48.5rem'
-        height='72.7rem'
+        objectFit='cover'
         src={MainIntroImage_1}
         alt='인트로'
-        className={twJoin('animate-fadeIn', count === 0 ? 'block' : 'hidden')}
+        className='h-[48.rem] w-[59.7rem] mobile:h-[38rem] mobile:w-[28.8rem] tablet:h-[48.7rem] tablet:w-[27.7rem]'
       />
-      <Image
-        width='48.5rem'
-        height='72.7rem'
-        src={MainIntroImage_2}
-        alt='인트로'
-        className={twJoin('animate-fadeIn', count === 1 ? 'block' : 'hidden')}
-      />
-      <div className='flex w-[60rem] flex-col gap-[3.3rem] text-common-black'>
-        <h3 className='text-[4rem] font-extrabold'>WITH SAILING STONE.</h3>
-        <div className='relative'>
-          <AnimatePresence>
-            <motion.p
-              key={count}
-              initial={{ opacity: 0 }}
-              animate={{ opacity: 1 }}
-              exit={{ opacity: 0 }}
-              transition={{ duration: 0.5 }}
-              className='absolute text-[3.5rem] font-medium'
-            >
-              {CONTENT[count]}
-            </motion.p>
-          </AnimatePresence>
-        </div>
-      </div>
     </section>
   );
 };
