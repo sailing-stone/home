52b1c57b4ae420f9674577ed00777ac41ae97e99
dhgg321 <ihji@sailingstone.io>
2025-02-07 16:59:29 +0900

    refactor: 보조 LNB 공용 컴포넌트로 분리

--- src/page/service.tsx
+++ src/page/service.tsx
@@ -1,16 +1,17 @@
 import ContactSection from '@/component/common/contact-section';
+import SubLNBNavigation from '@/component/common/sub-lnb-navigation';
 import AIService from '@/component/service/AI-service';
 import Banner from '@/component/service/banner';
 import BIConsulting from '@/component/service/bi-consulting';
 import BIEducation from '@/component/service/bi-education';
-import LocalNavigation from '@/component/service/local-navigation';
 import TBD from '@/component/service/TBD';
+import { SERVICE_NAVIGATION_LIST } from '@/constant/navigation-list';
 
 const ServicePage = () => {
   return (
     <div>
       <Banner />
-      <LocalNavigation />
+      <SubLNBNavigation navigationList={SERVICE_NAVIGATION_LIST} />
       <BIConsulting />
       <BIEducation />
       <TBD />
--- src/page/solution.tsx
+++ src/page/solution.tsx
@@ -1,15 +1,16 @@
 import ContactSection from '@/component/common/contact-section';
+import SubLNBNavigation from '@/component/common/sub-lnb-navigation';
 import AIPortal from '@/component/solution/AI-portal';
 import Banner from '@/component/solution/banner';
 import Dashboard from '@/component/solution/dashboard';
-import LocalNavigation from '@/component/solution/local-navigation';
 import TBD from '@/component/solution/TBD';
+import { SOLUTION_NAVIGATION_LIST } from '@/constant/navigation-list';
 
 const SolutionPage = () => {
   return (
     <div>
       <Banner />
-      <LocalNavigation />
+      <SubLNBNavigation navigationList={SOLUTION_NAVIGATION_LIST} />
       <AIPortal />
       <Dashboard />
       <TBD />
