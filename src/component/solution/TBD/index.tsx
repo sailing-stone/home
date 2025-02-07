5fd4c68b47dd1c7ba7609bb7bc1a490b6f4b73fe
dhgg321 <ihji@sailingstone.io>
2025-02-07 14:36:31 +0900

    refactor: TBD 문구 삭제

--- src/component/solution/TBD/index.tsx
+++ src/component/solution/TBD/index.tsx
@@ -29,9 +29,7 @@ const TBD = () => {
       className='flex flex-col items-center justify-center bg-contrast-200 pb-[12.9rem] pt-[8.8rem] mobile:hidden tablet:hidden'
     >
       <Badge>
-        <h1 className='section-title text-common-white'>
-          WEB / MOBILE APP (TBD)
-        </h1>
+        <h1 className='section-title text-common-white'>WEB / MOBILE APP</h1>
       </Badge>
       <p className='section-description text-common-white'>
         스마트 시대를 선도하는 맞춤형 앱 개발
