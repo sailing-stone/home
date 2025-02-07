d483dc6cac04de691d538bde21afbeb73e2c13ce
dhgg321 <ihji@sailingstone.io>
2025-02-07 14:34:50 +0900

    refactor: 데이터 프렙 삭제

--- src/constant/navigation-list.ts
+++ src/constant/navigation-list.ts
@@ -13,15 +13,14 @@ export const SERVICE_NAVIGATION_LIST = [
 ];
 
 export const SOLUTION_NAVIGATION_LIST = [
-  { id: 1, text: '데이터 AI 포털', href: URL.SOLUTION },
-  { id: 2, text: '대시보드 on TV', href: URL.SOLUTION },
+  { id: 1, hash: 'aiPortal', text: '데이터 AI 포털', href: URL.SOLUTION },
+  { id: 2, hash: 'dashboard', text: '대시보드 on TV', href: URL.SOLUTION },
   { id: 3, text: 'AI Agent', href: URL.SOLUTION },
-  { id: 4, text: '데이터 프랩', href: URL.SOLUTION },
-  { id: 5, text: 'Stone Excel', href: URL.SOLUTION },
-  { id: 6, text: '웹 / 모바일', href: URL.SOLUTION },
-  { id: 7, text: '커스텀 데이터 커넥터', href: URL.SOLUTION },
-  { id: 8, text: 'RPA', href: URL.SOLUTION },
-  { id: 9, text: 'Tableau Extension', href: URL.SOLUTION },
+  { id: 4, text: 'Stone Excel', href: URL.SOLUTION },
+  { id: 5, text: '웹 / 모바일', href: URL.SOLUTION },
+  { id: 6, text: '커스텀 데이터 커넥터', href: URL.SOLUTION },
+  { id: 7, text: 'RPA', href: URL.SOLUTION },
+  { id: 8, text: 'Tableau Extension', href: URL.SOLUTION },
 ];
 
 export const GNB_LIST = [
