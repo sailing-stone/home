ce7314818f06ccb650053ad8e907f201f1078e4c
dhgg321 <ihji@sailingstone.io>
2025-02-13 14:29:42 +0900

    fix: 대표번호 사무실 번호로 수정

--- src/component/layout/footer/index.tsx
+++ src/component/layout/footer/index.tsx
@@ -1,14 +1,7 @@
-import { useLocation } from 'react-router-dom';
 import LogoImage from '@/asset/image/logo-footer.png';
 import LogoWithTitle from '@/component/common/logo-with-title';
 
 const Footer = () => {
-  const { pathname } = useLocation();
-
-  if (pathname !== '/' && pathname !== '/service' && pathname !== '/contact') {
-    return null;
-  }
-
   return (
     <footer className='flex flex-col gap-[2.4rem] bg-contrast-200 px-40 pb-32 pt-16 mobile:gap-[1.6rem] mobile:px-[2.4rem] mobile:pb-12 mobile:pt-8 tablet:gap-8 tablet:px-[2.4rem] tablet:pb-16 tablet:pt-12'>
       <LogoWithTitle
@@ -27,7 +20,7 @@ const Footer = () => {
           두산더랜드 B동 1312호
         </p>
         <p>
-          | 대표번호 : 000-0000-0000 <br className='hidden mobile:block' />|
+          | 사무실번호 : 02-2039-9268 <br className='hidden mobile:block' />|
           이메일 : asdf@asdfasdfasdf.com
         </p>
       </div>
